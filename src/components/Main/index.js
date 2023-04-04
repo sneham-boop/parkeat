e39655207dc08f9a5b235bb7d712fb4de5ba2de2
Sneha Mahajan <sneh.km@gmail.com>
2023-04-04 16:26:55 -0400

    Add font for main page.

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,17 @@
 import BackgroundVideo from "../BackgroundVideo";
 import Footer from "../Footer";
 import Header from "../Header";
-
+import styles from "./Main.module.scss";
 
 export default function Main({ children }) {
   return (
     <>
-      <Header />
-      {children}
-      <Footer />
-      <BackgroundVideo/>
+      <div className={styles.main}>
+        <Header />
+        {children}
+        <Footer />
+        <BackgroundVideo />
+      </div>
     </>
   );
 }
