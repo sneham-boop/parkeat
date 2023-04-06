df8241d4411ecb459766b600ceac4cf91c8eb764
Sneha Mahajan <sneh.km@gmail.com>
2023-04-06 14:55:47 -0400

    Add dummy image for place.

--- src/components/ShowPlaces/Place.jsx
+++ src/components/ShowPlaces/Place.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import styles from "./ShowPlaces.module.scss"
+import styles from "./ShowPlaces.module.scss";
 
-export default function Place ({ place, image }) {
+export default function Place({ place, image }) {
   return (
     <section className={styles.place}>
-      {/* {image && <img alt="The places that were found" className={styles["place-image"]} src={image.html_attributions[0].href}/>} */}
-      <h4>{place.num}. {place.name}</h4>
-      <p>Address: {place.vicinity}</p>
-      <p>Rating: {place.rating}</p>
+      <img
+        alt="The places that were found"
+        className={styles["place-image"]}
+        src="https://images.pexels.com/photos/4062272/pexels-photo-4062272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      />
+      <div>
+        <h4>
+          {place.num}. {place.name}
+        </h4>
+        <p>Address: {place.vicinity}</p>
+        <p>Rating: {place.rating}</p>
+      </div>
     </section>
   );
-};
-
+}
