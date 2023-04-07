d5429eb466d4f4f4d00995f91600d1d0e5580f72
Sneha Mahajan <sneh.km@gmail.com>
2023-04-07 16:04:56 -0400

    Add image through Image component from nextjs

--- src/components/ShowPlaces/Place.jsx
+++ src/components/ShowPlaces/Place.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./ShowPlaces.module.scss";
+import Image from "next/image";
 
 export default function Place({ place, image }) {
   return (
     <section className={styles.place}>
-      <img
+      {/* <img
         alt="The places that were found"
         className={styles["place-image"]}
+        src="https://images.pexels.com/photos/4062272/pexels-photo-4062272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      /> */}
+      <Image
+        alt="The places that were found"
+        // className={styles["place-image"]}
+        width={200}
+        height={200}
         src="https://images.pexels.com/photos/4062272/pexels-photo-4062272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
       />
       <div>
