b26944d87a4fafdc4ef08ad183754409dbb4c46f
Sneha Mahajan <sneh.km@gmail.com>
2023-04-12 18:21:12 -0400

    Change font usage to next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import "@component/styles/globals.css";
 import Main from "@component/src/components/Main";
+import { Work_Sans } from "next/font/google";
 
+const work = Work_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Main>
-        <Component {...pageProps} />
-      </Main>
+      <main className={work.className}>
+        <Main>
+          <Component {...pageProps} />
+        </Main>
+      </main>
     </>
   );
 }
