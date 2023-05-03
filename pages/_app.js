5cd13399c1febd9177e915a896d286358a8ffd7c
Sneha Mahajan <sneh.km@gmail.com>
2023-05-03 15:15:49 -0400

    Use Google's map component wrapper instead of google-maps-react package

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "@component/styles/globals.css";
 import Main from "@component/src/components/Main";
 import { Work_Sans } from "next/font/google";
 
-const work = Work_Sans({ subsets: ["latin"] });
+const work = Work_Sans({ weight: ["100", "200", "400", "500", "800", "900"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -1,109 +1,128 @@
-import React, { useState, useEffect } from "react";
-import GoogleMapReact from "google-map-react";
-import useLocalStorage from "../../hooks/useLocalStorage";
-import useGoogleMapsAPI from "../../hooks/useGoogleMapsAPI";
-import Markers from "./Markers";
+import React, { useState, useEffect, useRef } from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import styles from "./GoogleMap.module.scss";
+import { createRoot } from "react-dom/client";
 
-export default function GoogleMap(props) {
-  const { getLocalStorage, setLocalStorage } = useLocalStorage();
-  const { getAPIKey } = useGoogleMapsAPI();
-  const [key, setKey] = useState(null);
+const render = (status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <h2>Loading</h2>;
 
-  // Show restaurants if they exist
-  const { restaurants } = props;
-  const showPlaces = (places) => {
-    const placesArray = Object.values(places);
-    return placesArray.map((place, id) => (
-      <Markers
-        key={id}
-        id={id}
-        name={place.name}
-        description="Restaurant"
-        lat={place.geometry.location.lat}
-        lng={place.geometry.location.lng}
-      />
-    ));
-  };
+    case Status.FAILURE:
+      return <h2>Failure</h2>;
+
+    case Status.SUCCESS:
+      return <MyMap />;
+  }
+};
+
+export default function GoogleMapp() {
   const defaultLocation = { lat: 43.6532, lng: -79.3832 };
-  const [location, setLocation] = useState(
-    getLocalStorage("location") || defaultLocation
-  );
-  // const [mapAPILoaded, setMapAPILoaded] = useState(false);
-  // const [map, setMap] = useState(null);
-  // const [mapAPI, setMapAPI] = useState(null);
-
-  const getCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      setLocation({ lat: latitude, lng: longitude });
-      setLocalStorage("location", { lat: latitude, lng: longitude });
-    });
-  };
-
-  // Current location setting
-  useEffect(() => {
-    getCurrentLocation();
-    return function cleanup() {
-      getCurrentLocation();
-    };
-  });
-
-  // Maps api key setting
-  useEffect(() => {
-    getAPIKey().then((key) => {
-      setKey(key);
-    });
-  }, [getAPIKey]);
-
-  // const handleApiLoaded = (map, maps) => {
-  //   setMapAPILoaded(true);
-  //   setMap(map);
-  //   setMapAPI(maps);
-  // };
-
-  // const data = {
-  //   map,
-  //   mapAPI,
-  //   mapAPILoaded,
-  //   location,
-  // };
 
   return (
-    <div className={styles.mapContainer}>
+    <>
       <h2>Let&apos;s find some restaurants for you.</h2>
-      <div
-        id={styles.map}
-        style={{
-          width: "80vw",
-          height: "50vh",
-          borderRadius: "1.5rem",
-          overflow: "hidden",
-        }}
+      <Wrapper
+        apiKey={process.env.NEXT_PUBLIC_MAP_API_KEY}
+        version="beta"
+        libraries={["marker"]}
+        render={render}
       >
-        {key && (
-          <GoogleMapReact
-            bootstrapURLKeys={{
-              key: key,
-              libraries: ["places"],
-            }}
-            defaultCenter={defaultLocation}
-            center={location}
-            zoom={15}
-            yesIWantToUseGoogleMapApiInternals
-            // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-            {...props}
-          >
-            <Markers
-              lat={location.lat}
-              lng={location.lng}
-              id="Me"
-              description="You are here!"
-            />
-            {restaurants && showPlaces(restaurants)}
-          </GoogleMapReact>
-        )}
-      </div>
-    </div>
+        <MyMap center={defaultLocation} zoom={15} />
+      </Wrapper>
+    </>
+  );
+}
+
+const placeData = {
+  A: {
+    name: "Bob's Burgers",
+    position: { lat: 43.7027104, lng: -79.3996452 },
+    rating: 4.5,
+    cuisine: "American",
+  },
+  B: {
+    name: "Bob's Bananas",
+    position: { lat: 43.629669,lng: -80.1018147 },
+    rating: 4.5,
+    cuisine: "American",
+  },
+  C: {
+    name: "Bob's Cakes",
+    position: { lat: 43.497006, lng: -79.874574 },
+    rating: 4.5,
+    cuisine: "American",
+  },
+};
+
+function Marker({ map, children, position }) {
+  const markerRef = useRef();
+  const rootRef = useRef();
+
+  useEffect(() => {
+    if (!rootRef.current) {
+      const container = document.createElement("div");
+      rootRef.current = createRoot(container);
+      markerRef.current = new google.maps.marker.AdvancedMarkerView({
+        position,
+        content: container,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    rootRef.current.render(children);
+    markerRef.current.position = position;
+    markerRef.current.map = map;
+  }, [map, position, children]);
+}
+
+function Places({ map }) {
+  const [data, setData] = useState(placeData);
+
+  const showPlaces = () => {
+    return Object.entries(data).map(([key, place]) => {
+      return (
+        <Marker key={key} map={map} position={place.position}>
+          <div className={styles.marker}>
+            <p>{place.name}</p>
+          </div>
+        </Marker>
+      );
+    });
+  };
+
+  return <>{showPlaces()}</>;
+}
+
+function MyMap({ center, zoom }) {
+  const ref = useRef();
+  const [map, setMap] = useState();
+  const defaultLocation = { lat: 43.6532, lng: -79.3832 };
+
+  const mapOptions = {
+    mapId: process.env.NEXT_PUBLIC_MAP_ID,
+    center: defaultLocation,
+    zoom: 10,
+    disableDefaultUI: true,
+  };
+
+  useEffect(() => {
+    setMap(new window.google.maps.Map(ref.current, mapOptions));
+  }, []);
+
+  return (
+    <>
+      <div
+        className={styles.mapContainer}
+        ref={ref}
+        id="map"
+        style={{
+          width: "100vw",
+          height: "100vh",
+        }}
+      />
+      {map && <Places map={map} />}
+    </>
   );
 }
