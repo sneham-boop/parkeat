fbef03e2e005cc8ccbdc0b1bdc579285cb728bf3
Sneha Mahajan <sneh.km@gmail.com>
2023-05-03 18:52:16 -0400

    Display image of place looked up.

--- src/components/ShowPlaces/Place.jsx
+++ src/components/ShowPlaces/Place.jsx
@@ -6,22 +6,17 @@ import Button from "../Button";
 export default function Place({ place, image }) {
   return (
     <section className={styles.place}>
-      {/* <Image
-        alt="The places that were found"
-        // className={styles["place-image"]}
-        width={200}
-        height={200}
-        src="https://images.pexels.com/photos/4062272/pexels-photo-4062272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        priority
-      /> */}
+      <img
+        className={styles.placeImage} src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=100&photo_reference=${image.photo_reference}&key=${process.env.NEXT_PUBLIC_MAP_API_KEY}`}
+      ></img>
       <div>
         <h4>
           {place.num}. {place.name}
         </h4>
         <p>Address: {place.vicinity}</p>
         <p>Rating: {place.rating}</p>
-        <Button btnText="Save"/>
-        <Button btnText="Remove"/>
+        {/* <Button btnText="Save"/>
+        <Button btnText="Remove"/> */}
       </div>
     </section>
   );
