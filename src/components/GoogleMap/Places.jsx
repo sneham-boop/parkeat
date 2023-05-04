e992fe8ce3bbdab51dc83e605f347cd7f8e1bff5
Sneha Mahajan <sneh.km@gmail.com>
2023-05-04 14:14:49 -0400

    Fix width of markers

--- src/components/GoogleMap/Places.jsx
+++ src/components/GoogleMap/Places.jsx
@@ -5,24 +5,27 @@ import { useState } from "react";
 export default function Places({ map, data, parkingData }) {
   // const [data, setData] = useState(placeData);
   const [hover, setHover] = useState(null);
-console.log(data)
+
   const showPlaces = () => {
     return data.map((place, key) => {
       // console.log(place.geometry.location);
       return (
-        <Marker key={place.place_id} map={map} position={place.geometry.location}>
+        <Marker
+          key={place.place_id}
+          map={map}
+          position={place.geometry.location}
+        >
           <div
-            className={`${styles.marker} ${hover === (place.place_id) ? styles.hover : ""}`}
+            className={`${styles.marker} ${
+              hover === place.place_id ? styles.hover : ""
+            }`}
             onMouseEnter={() => setHover(place.place_id)}
             onMouseLeave={() => setHover(null)}
             onClick={() => console.log("I was clicked.", place.place_id)}
             style={{ backgroundColor: "#FF8A65" }}
           >
             <img src="/restaurantIcon.svg"></img>
-            <span>
-              {" "}
-              {key + 1}. {place.name}
-            </span>
+            {hover === place.place_id && <span>{place.name}</span>}
           </div>
         </Marker>
       );
@@ -32,19 +35,24 @@ console.log(data)
     return parkingData.map((place, key) => {
       // console.log(place.geometry.location);
       return (
-        <Marker key={place.place_id} map={map} position={place.geometry.location}>
+        <Marker
+          key={place.place_id}
+          map={map}
+          position={place.geometry.location}
+        >
           <div
-            className={`${styles.marker} ${hover === (place.place_id) ? styles.hover : ""}`}
+            className={`${styles.marker} ${
+              hover === place.place_id ? styles.hover : ""
+            }`}
             onMouseEnter={() => setHover(place.place_id)}
             onMouseLeave={() => setHover(null)}
             onClick={() => console.log("I was clicked.", place.place_id)}
             style={{ backgroundColor: "#242C2E" }}
           >
             <img src="/garageIcon.svg"></img>
-            <span>
-              {" "}
-              {key + 1}. {place.name}
-            </span>
+            {hover === place.place_id && (
+              <span>{hover === place.place_id && place.name}</span>
+            )}
           </div>
         </Marker>
       );
