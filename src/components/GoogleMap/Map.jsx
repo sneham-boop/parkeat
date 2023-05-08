ba21c9eb3513b537319e0dd6d45b27d7171d9072
Sneha Mahajan <sneh.km@gmail.com>
2023-05-08 10:32:42 -0400

    Fix when current position is pulled from browser.

--- src/components/GoogleMap/Map.jsx
+++ src/components/GoogleMap/Map.jsx
@@ -2,25 +2,38 @@ import styles from "./GoogleMap.module.scss";
 import Places from "./Places";
 import { useRef, useEffect, useState } from "react";
 
-export default function Map({ data, center, parkingData }) {
+export default function Map({ data, parkingData }) {
   const ref = useRef();
   const [map, setMap] = useState();
-  const defaultLocation = { lat: 43.655484, lng: -79.38611 };
+  const [center, setCenter] = useState({ lat: 43.655484, lng: -79.38611 })
+  // const defaultLocation = { lat: 43.655484, lng: -79.38611 };
 
-  const mapOptions = {
-    mapId: process.env.NEXT_PUBLIC_MAP_ID,
-    center: center || defaultLocation,
-    zoom: 14,
-    disableDefaultUI: true,
-  };
+  // const mapOptions = {
+  //   mapId: process.env.NEXT_PUBLIC_MAP_ID,
+  //   center: center || defaultLocation,
+  //   zoom: 14,
+  //   disableDefaultUI: true,
+  // };
 
   useEffect(() => {
-    setMap(new window.google.maps.Map(ref.current, mapOptions));
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      // setLocation({ lat: latitude, lng: longitude });
+      setCenter({ lat: latitude, lng: longitude })
+      const mapOptions = {
+        mapId: process.env.NEXT_PUBLIC_MAP_ID,
+        center: { lat: latitude, lng: longitude },
+        zoom: 14,
+        disableDefaultUI: true,
+      };
+      setMap(new window.google.maps.Map(ref.current, mapOptions));
+    });
+    
   }, []);
 
-  useEffect(()=>{
-    map && map.setCenter(center); 
-  },[center])
+  // useEffect(()=>{
+  //   map && map.setCenter(center); 
+  // },[center])
 
   return (
     <>
